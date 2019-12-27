802f04b479a831e733c2ab43284f5c07f0fd8fcc
Phong Dat <phongdat.pirago@gmail.com>
2019-12-27 11:45:20 +0700

    all

---
+++ src/test/java/com/phongdat/pirago/AwsElasticBeanstalkExample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phongdat.pirago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstalkExample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
