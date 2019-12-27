d137e1a8ba078cc6306548a92da4acd3a5244760
Phong Dat <phongdat.pirago@gmail.com>
2019-12-27 13:41:29 +0700

    all

--- src/main/java/com/phongdat/pirago/HelloController.java
+++ src/main/java/com/phongdat/pirago/HelloController.java
@@ -1,11 +1,13 @@
 package com.phongdat.pirago;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+    @GetMapping
     public String hello() {
         return "Hello Pirago";
     }
